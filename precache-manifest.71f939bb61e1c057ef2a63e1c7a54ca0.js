dad2c20113689f9f4d0b62e3031c7d169ebcd29b
ecadet-mum <51096249+ecadet-mum@users.noreply.github.com>
2020-06-18 13:19:30 -0400

    Updates

---
+++ precache-manifest.71f939bb61e1c057ef2a63e1c7a54ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6eedb8659ca0b479880df0662a4907",
+    "url": "/testcadet/index.html"
+  },
+  {
+    "revision": "0d98ef485888c933187c",
+    "url": "/testcadet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "737d9b9a47a436e7bed8",
+    "url": "/testcadet/static/js/2.31711120.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testcadet/static/js/2.31711120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d98ef485888c933187c",
+    "url": "/testcadet/static/js/main.e4a0f155.chunk.js"
+  },
+  {
+    "revision": "d1cb6577c746962aae43",
+    "url": "/testcadet/static/js/runtime-main.d4175044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testcadet/static/media/logo.5d5d9eef.svg"
+  }
+]);
