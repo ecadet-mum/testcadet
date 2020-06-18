0823f949395559d536618fbbd0377c50acb9368d
ecadet-mum <51096249+ecadet-mum@users.noreply.github.com>
2020-06-18 13:25:14 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [inputValue,setInputValue]=useState('')
+
+  const handleClick=()=>{
+    alert(inputValue);
+  }
+
+  const changeInputValue=(ev)=>{
+     setInputValue(ev.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={changeInputValue}/><input type='button' value='Add' onClick={handleClick}/>
     </div>
   );
 }
 
 export default App;
+
